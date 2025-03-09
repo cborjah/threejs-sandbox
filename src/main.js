bc215a9a33a24e3a4bfb1f2819127835e081aa32
Christian Borja <cpborja32@gmail.com>
2025-03-08 17:46:24 -0800

    feat: create basic animated scene

---
+++ src/main.js
@@ -0,0 +1,87 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+import GUI from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+/*********
+ * Sizes *
+ *********/
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+/********
+ * Base *
+ ********/
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**********
+ * Models *
+ **********/
+
+// Box
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+/**********
+ * Camera *
+ **********/
+
+// Base Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+
+camera.position.z = 3;
+scene.add(camera);
+
+/************
+ * Controls *
+ ************/
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/************
+ * Renderer *
+ ************/
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+});
+renderer.setSize(sizes.width, sizes.height);
+
+/***********
+ * Animate *
+ ***********/
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+
+    previousTime = elapsedTime;
+
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
