3c3116b46070588dc0cf25654ddeb6f817314164
Christian Borja <cpborja32@gmail.com>
2025-03-20 16:12:05 -0700

    feat: add toggle for adding and removing particles from scene

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -6,12 +6,42 @@ export default class World {
     constructor() {
         this.experience = new Experience();
         this.resources = this.experience.resources;
+        this.scene = this.experience.scene;
+
+        // Debug
+        this.debug = this.experience.debug;
+
+        if (this.debug) {
+            this.setDebugFolders();
+        }
 
         // Setup
         this.box = new Box();
 
-        this.resources.on("ready", () => {
-            this.particles = new Particles();
-        });
+        // this.resources.on("ready", () => {
+        // if (this.worldDebugOptions.particles) {
+        // this.particles = new Particles();
+        // }
+        // });
+    }
+
+    setDebugFolders() {
+        this.debugWorldFolder = this.debug.gui.addFolder("World");
+
+        this.worldDebugOptions = {
+            box: true,
+            particles: false
+        };
+
+        this.debugWorldFolder
+            .add(this.worldDebugOptions, "particles")
+            .onChange((isEnabled) => {
+                if (isEnabled) {
+                    this.particles = new Particles();
+                } else if (this.particles) {
+                    this.particles.destroy();
+                    this.particles = null;
+                }
+            });
     }
 }
