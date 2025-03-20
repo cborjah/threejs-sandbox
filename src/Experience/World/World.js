a00ddc62e518313da4e931601321d9d28ddc5c8f
Christian Borja <cpborja32@gmail.com>
2025-03-19 20:44:26 -0700

    feat: wait for resources to load before adding particles to scene

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,10 +1,17 @@
+import Experience from "../Experience";
 import Box from "./Box";
 import Particles from "./Particles";
 
 export default class World {
     constructor() {
+        this.experience = new Experience();
+        this.resources = this.experience.resources;
+
         // Setup
         this.box = new Box();
-        this.particles = new Particles();
+
+        this.resources.on("ready", () => {
+            this.particles = new Particles();
+        });
     }
 }
