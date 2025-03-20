638a30a75e48721e0253997da28d5fd75aad439a
Christian Borja <cpborja32@gmail.com>
2025-03-19 20:23:59 -0700

    refactor: move instantiation logic for particles from Experience to World

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,8 +1,10 @@
 import Box from "./Box";
+import Particles from "./Particles";
 
 export default class World {
     constructor() {
         // Setup
         this.box = new Box();
+        this.particles = new Particles();
     }
 }
