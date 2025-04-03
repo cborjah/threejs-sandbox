1ebb6282e6f6848307004d373c166634853e477f
Christian Borja <cpborja32@gmail.com>
2025-04-02 21:22:18 -0700

    refactor: move PhysicsBox logic into Box class, delete PhysicsBox file

--- src/Experience/World/Box.js
+++ src/Experience/World/Box.js
@@ -1,19 +1,31 @@
 import * as THREE from "three";
+import RAPIER from "@dimforge/rapier3d";
 
 import Experience from "../Experience";
 
 export default class Box {
-    constructor(position) {
+    constructor({ dimensions, position = [0, 0, 0], physics = false } = {}) {
+        if (!dimensions) {
+            throw new Error("Unable to create Box, missing dimensions...");
+        }
+
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.world = this.experience.world;
 
-        this.dimensions = [1, 1, 1];
-        this.position = position || [0, 0, 0];
+        this.dimensions = dimensions;
+        this.position = position;
+        this.physics = physics;
 
         // Setup
         this.setGeometry();
         this.setMaterial();
         this.setMesh();
+
+        if (this.physics) {
+            this.setRigidBody();
+            this.setCollider();
+        }
     }
 
     setGeometry() {
@@ -29,5 +41,27 @@ export default class Box {
         this.mesh.position.set(...this.position);
 
         this.scene.add(this.mesh);
+    }
+
+    setRigidBody() {
+        this.rigidBody = this.world.rapier.createRigidBody(
+            RAPIER.RigidBodyDesc.dynamic()
+                .setTranslation(...this.position)
+                .setCanSleep(false)
+        );
+    }
+
+    setCollider() {
+        const colliderDimensions = this.dimensions.map((value) => value / 2);
+
+        this.collider = RAPIER.ColliderDesc.cuboid(...colliderDimensions)
+            .setMass(1)
+            .setRestitution(1.1);
+
+        this.world.rapier.createCollider(this.collider, this.rigidBody);
+    }
+
+    animate() {
+        this.mesh.position.copy(this.rigidBody.translation());
     }
 }
